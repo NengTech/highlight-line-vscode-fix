6618e5ba5c61c098313398026766c309cdd7e7aa
alicepolice <1727235919@qq.com>
2026-03-29 03:29:04 +0800

    修复无条件访问 activeEditor.selection，但 activeEditor 有时会是 undefined 的问题，老项目迁移新项目，修改 package.json，重新编译打包

--- README.md
+++ README.md
@@ -1,55 +1,26 @@
-[![](https://vsmarketplacebadge.apphb.com/version/cliffordfajardo.highlight-line-vscode.svg)](https://marketplace.visualstudio.com/items?itemName=cliffordfajardo.highlight-line-vscode)
-[![](https://vsmarketplacebadge.apphb.com/installs-short/cliffordfajardo.highlight-line-vscode.svg)](https://marketplace.visualstudio.com/items?itemName=cliffordfajardo.highlight-line-vscode)
-[![](https://vsmarketplacebadge.apphb.com/rating-short/cliffordfajardo.highlight-line-vscode.svg)](https://marketplace.visualstudio.com/items?itemName=cliffordfajardo.highlight-line-vscode)
+# 修复无条件访问 activeEditor.selection，但 activeEditor 有时会是 undefined 的问题
+
 # Highlight Line VS Code
 
 Enhanced highlighting of the currently active line in your text editor.
 
-
-## Features 
+## Features
 
 ### Multi-Tab Line Highlighting
 
 Line highlighting is provided & preserved across multiple tabs, so it's easier to find where you left off in a tab.
 
->![Highlight Line VScode preview](images/highlight-line-preview1.png)
-
-
 ## Customizable Settings
 
-Through the configuration options, you can edit appearance of your hilighted line and see the changes take effect immediately in your editor. 
-
->![Highlight Line VScode customization preview](images/highlight-line.gif)
-
+Through the configuration options, you can edit appearance of your hilighted line and see the changes take effect immediately in your editor.
 
 <table>
   <tr>
     <th colspan="2">Highlight Line Settings</th>
   </tr>
 
-  <tr align="left">
-    <th>Name</th>
-    <th>Description</th>
-  </tr>
-
-  <tr>
-    <td><code>highlightLine.borderColor</code></td>
-    <td>Specifies the border color used & is changable to any valid CSS color value. For example here are a list of valid values: `'red'`, `'#FFF'` `'#FFFFFFF'`, `'RGB(255,255,255)'`, `'RGB(255, 255, 255. 0.5)'`</td>
-  </tr>
-  <tr>
-    <td><code>highlightLine.borderWidth</code></td>
-    <td>Specifies the width of the border in pixels For example: `'2px'`</></td>
-  </tr>
-  <tr>
-    <td><code>highlightLine.borderStyle</code></td>
-    <td>Specifies the border style of the line. For example, here a a list of valid values: `solid`, `dashed`, `inset`, `double`, `groove`, `outset`, `ridge`</td>
-  </tr>
-  </table>
-
-
 ### Thank You
+
 > Thanks checking out my extension! I'm mostly hang out on [twitter](https://twitter.com/cliffordfajard0), ask me questions or let's talk!
 
-<a href="https://twitter.com/cliffordfajard0"><img src="https://img.shields.io/twitter/follow/cliffordfajard0?style=social" alt="Twitter Profile Badge"/></a>
-
-
+`<a href="https://twitter.com/cliffordfajard0"><img src="https://img.shields.io/twitter/follow/cliffordfajard0?style=social" alt="Twitter Profile Badge"/>``</a>`
--- src/extension.ts
+++ src/extension.ts
@@ -1,107 +1,137 @@
-
 'use strict'
-import { commands, ExtensionContext, window, workspace, Range, Position} from 'vscode'
-import { setTimeout } from 'timers';
+import {
+    ExtensionContext,
+    window,
+    workspace,
+    Range,
+    Position,
+    TextEditor,
+    TextEditorDecorationType,
+} from 'vscode'
 
 export async function activate(context: ExtensionContext) {
-    /*NOTE: we should only create 1 declaration type object once, as done here.
-      If we recreate it in 'updateDecorations' each time, when we removeDecorations,
-      they will reference diff objects.
-    */
     let decorationType = getDecorationTypeFromConfig();
-    let activeEditor = window.activeTextEditor;
-    let lastActivePosition;
+    let activeEditor: TextEditor | undefined = window.activeTextEditor;
+    let lastActivePosition: Position | undefined = activeEditor
+    ? activeEditor.selection.active
+    : undefined;
 
-
-    /**
-     * This is required. When we create a new tab in our editor, we want
-     * to update the activeEditor.
-     */
-    window.onDidChangeActiveTextEditor(() => {
-        try {
-            activeEditor = window.activeTextEditor
-            updateDecorations(decorationType)
-        } catch (error){
-            console.error("Error from ' window.onDidChangeActiveTextEditor' -->", error)
-        } finally {
-            lastActivePosition = new Position(activeEditor.selection.active.line, activeEditor.selection.active.character);
+    function syncLastActivePosition(editor: TextEditor | undefined) {
+        if (!editor) {
+            lastActivePosition = undefined;
+            return;
         }
-    })
+        lastActivePosition = editor.selection.active;
+    }
 
-    /**
-     * Any time we move anywhere around our editor, we want to trigger
-     * a decoration.
-     */
-    window.onDidChangeTextEditorSelection(() => {
-        activeEditor = window.activeTextEditor;   
-        updateDecorations(decorationType);
-    })
-    /**
-     * 
-     * @param decorationType - defines our decorations settings.
-     */
-    function updateDecorations(decorationType, updateAllVisibleEditors=false) {
+    function updateDecorations(
+        decorationType: TextEditorDecorationType,
+        updateAllVisibleEditors = false
+    ) {
         try {
             if (updateAllVisibleEditors) {
                 window.visibleTextEditors.forEach((editor) => {
                     const currentPosition = editor.selection.active;
-                    const currentLine = editor.selection.active.line;
-                    const newDecoration = { range: new Range(currentPosition, currentPosition) };
+                    const newDecoration = {
+                        range: new Range(currentPosition, currentPosition),
+                    };
                     editor.setDecorations(decorationType, [newDecoration]);
                 });
+                return;
             }
 
-            //edit only currently active editor
-            else {
-                window.visibleTextEditors.forEach((editor) => {
-                    if(editor !== window.activeTextEditor) return;
-                    
-                    const currentPosition = editor.selection.active
-                    const editorHasChangedLines = lastActivePosition.line !== currentPosition.line
-                    const isNewEditor = activeEditor.document.lineCount === 1 && lastActivePosition.line === 0 && lastActivePosition.character == 0;
-                    const newDecoration = { range: new Range(currentPosition, currentPosition) }
-                    
-                    if(editorHasChangedLines || isNewEditor){
-                        editor.setDecorations(decorationType, [newDecoration])
-                    }
-                });
+            const editor = activeEditor;
+            if (!editor) {
+                return;
             }
-        } 
-        catch (error){
-            console.error("Error from ' updateDecorations' -->", error)
-        } finally {
-            lastActivePosition = new Position(activeEditor.selection.active.line, activeEditor.selection.active.character);
+
+            const currentPosition = editor.selection.active;
+
+            // 第一次进入时，先初始化
+            if (!lastActivePosition) {
+                const newDecoration = {
+                    range: new Range(currentPosition, currentPosition),
+                };
+                editor.setDecorations(decorationType, [newDecoration]);
+                lastActivePosition = currentPosition;
+                return;
+            }
+
+            const editorHasChangedLines =
+                lastActivePosition.line !== currentPosition.line;
+
+            const isNewEditor =
+                editor.document.lineCount === 1 &&
+                lastActivePosition.line === 0 &&
+                lastActivePosition.character === 0;
+
+            if (editorHasChangedLines || isNewEditor) {
+                const newDecoration = {
+                    range: new Range(currentPosition, currentPosition),
+                };
+                editor.setDecorations(decorationType, [newDecoration]);
+            }
+
+            lastActivePosition = currentPosition;
+        } catch (error) {
+            console.error("Error from 'updateDecorations' -->", error);
         }
-
-
     }
 
-    workspace.onDidChangeConfiguration(() => {
-        //clear all decorations
-        decorationType.dispose();
-        decorationType = getDecorationTypeFromConfig();
-        updateDecorations(decorationType, true)
-    })
+    context.subscriptions.push(
+        window.onDidChangeActiveTextEditor((editor) => {
+            try {
+                activeEditor = editor;
+
+                if (!editor) {
+                    lastActivePosition = undefined;
+                    return;
+                }
+
+                updateDecorations(decorationType);
+            } catch (error) {
+                console.error("Error from 'window.onDidChangeActiveTextEditor' -->", error);
+            } finally {
+                syncLastActivePosition(editor);
+            }
+        })
+    );
+
+    context.subscriptions.push(
+        window.onDidChangeTextEditorSelection((event) => {
+            try {
+                activeEditor = event.textEditor;
+                updateDecorations(decorationType);
+            } catch (error) {
+                console.error("Error from 'window.onDidChangeTextEditorSelection' -->", error);
+            } finally {
+                syncLastActivePosition(event.textEditor);
+            }
+        })
+    );
+
+    context.subscriptions.push(
+        workspace.onDidChangeConfiguration(() => {
+            decorationType.dispose();
+            decorationType = getDecorationTypeFromConfig();
+            updateDecorations(decorationType, true);
+        })
+    );
 }
 
-
-
-//UTILITIES
+// UTILITIES
 function getDecorationTypeFromConfig() {
-    const config = workspace.getConfiguration("highlightLine")
-    const borderColor = config.get("borderColor");
-    const borderWidth = config.get("borderWidth");
-    const borderStyle = config.get("borderStyle");
-    const decorationType = window.createTextEditorDecorationType({
+    const config = workspace.getConfiguration('highlightLine');
+    const borderColor = config.get<string>('borderColor');
+    const borderWidth = config.get<string>('borderWidth');
+    const borderStyle = config.get<string>('borderStyle');
+
+    return window.createTextEditorDecorationType({
         isWholeLine: true,
         borderWidth: `0 0 ${borderWidth} 0`,
-        borderStyle: `${borderStyle}`, //TODO: file bug, this shouldn't throw a lint error.
-        borderColor
-    })
-    return decorationType;
+        borderStyle: `${borderStyle}`,
+        borderColor,
+    });
 }
 
-
-// this method is called when your extension is deactivated
-export function deactivate() {
-}
+export function deactivate() {}
